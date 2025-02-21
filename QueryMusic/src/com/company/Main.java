fe4effdab6f3c77c89f263354e370d1729d212fd
ahmedali <ahmed.ali3509@gmail.com>
2025-02-20 19:11:06 -0500

    adding full CRUD features

--- QueryMusic/src/com/company/Main.java
+++ QueryMusic/src/com/company/Main.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
 
@@ -23,31 +24,35 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        String albumName = "Tapestry";
-        String query = "SELECT * FROM music.albumview WHERE album_name = '%s'".formatted(albumName);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an Album Name: ");
+        String artistId = scanner.nextLine();
+        int artistid = Integer.parseInt(artistId);
+        String query = "SELECT * FROM music.artists WHERE artist_id = %d".formatted(artistid);
 
         var dataSource = new MysqlDataSource();
         dataSource.setServerName(props.getProperty("serverName"));
         dataSource.setPort(Integer.parseInt(props.getProperty("port")));
         dataSource.setDatabaseName(props.getProperty("databaseName"));
 
+
         try (var connection  = dataSource.getConnection(props.getProperty("user"),System.getenv("MYSQL_PASS"));
              Statement statement = connection.createStatement();
              ) {
-            ResultSet resultSet = statement.executeQuery(query);
+                ResultSet resultSet = statement.executeQuery(query);
 
-            var meta = resultSet.getMetaData();
-            for (int i = 1; i <= meta.getColumnCount(); i++) {
-                System.out.printf("%d %s %s%n", i, meta.getColumnName(i), meta.getColumnTypeName(i));
-            }
-            System.out.println("========================");
-            while(resultSet.next()) {
+                var meta = resultSet.getMetaData();
+
                 for (int i = 1; i <= meta.getColumnCount(); i++) {
-                    System.out.printf("%-15s", resultSet.getString(i));
+                    System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
                 }
-                System.out.println();
-            }
 
+                while(resultSet.next()) {
+                    for (int i = 1; i <= meta.getColumnCount(); i++) {
+                        System.out.printf("%-15s", resultSet.getString(i));
+                    }
+                    System.out.println();
+                }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
