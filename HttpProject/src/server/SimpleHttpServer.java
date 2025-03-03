46ceb659ac1bda883b01ad0ed1a9a4b75ca24f2f
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-03-03 12:29:24 -0600

    adding HttpClient functionality

--- HttpProject/src/server/SimpleHttpServer.java
+++ HttpProject/src/server/SimpleHttpServer.java
@@ -4,6 +4,8 @@ import com.sun.net.httpserver.HttpsServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 
@@ -22,21 +24,34 @@ public class SimpleHttpServer {
 
                 String data = new String(exchange.getRequestBody().readAllBytes());
                 System.out.println("Body data: " + data);
+
+                Map<String, String> parameters = parseParameters(data);
+                System.out.println(parameters);
+
+                exchange.getRequestHeaders().entrySet().forEach(System.out::println);
                 if (requestMethod.equals("POST")) {
                     visitorCounter++;
                 }
 
+                String firstName = parameters.get("first");
+                String lastName = parameters.get("last");
                 String response = """
                         <html>
                             <body>
                                 <h1> Hello world from my http server </h1>
                                 <p> Number of Visitors who signed up = %d</p>
                                 <form method="post">
+                                    <label for="first>First name:</label>
+                                    <input type="text" id="first" name="first" value="%s">
+                                    <br>
+                                    <label for="last">Last name:</label>
+                                    <input type="text" id="last" name="last" value="%s">
+                                    <br>
                                     <input type="submit" value="Submit">
                                 </form>
                             </body>
                         </html>
-                        """.formatted(visitorCounter);
+                        """.formatted(visitorCounter, firstName == null ? "" : firstName, lastName == null ? "" : lastName);
 
 
                 var bytes = response.getBytes();
@@ -52,6 +67,19 @@ public class SimpleHttpServer {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static Map<String, String> parseParameters(String requestBody) {
+
+        Map<String, String> parameters = new HashMap<>();
+        String[] pairs = requestBody.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                parameters.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return parameters;
     }
 
 }
