9f3c466a1e7fa31ecd7baaf11142e1a98f16569e
ahmedali <ahmed.ali3509@gmail.com>
2025-04-02 09:33:46 -0400

    ThreeSum leetcode challenge

--- LeetCode/src/com/company/threeSum.java
+++ LeetCode/src/com/company/threeSum.java
@@ -1,49 +1,41 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class threeSum {
 
     public List<List<Integer>> three_sum(int[] nums) {
 
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums.length < 3) {
-            return result;
+        Set<List<Integer>> result = new HashSet<>();
+
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>(result);
         }
 
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            int target = -nums[i];
-            int[] twoSumsArray = nums[:i+1];
-            int[] twoSumResult = twoSum(nums[i], target);
 
-        }
+        for (int i = 0; i < nums.length - 2; i++) {
 
+            int left = i+1;
+            int right = nums.length-1;
 
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
 
-
-
-        return result;
-    }
-
-
-    public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-
-        while (left < right) {
-            int total = numbers[left] + numbers[right];
-
-            if (total == target) {
-                return new int[]{left + 1, right + 1};
-            } else if (total > target) {
-                right--;
-            } else {
-                left++;
             }
         }
-        return new int[]{-1, -1}; // If no solution is found
+
+        return new ArrayList<>(result);
+
     }
+
 }
