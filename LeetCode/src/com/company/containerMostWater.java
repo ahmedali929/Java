fbb5f39c6801e3a9b91ea231a0386a2c65135c1a
ahmedali <ahmed.ali3509@gmail.com>
2025-04-07 17:20:48 -0400

    pushing leetcode challenge updates

--- LeetCode/src/com/company/containerMostWater.java
+++ LeetCode/src/com/company/containerMostWater.java
@@ -1,2 +1,23 @@
-package com.company;public class containerMostWater {
+package com.company;
+
+public class containerMostWater {
+
+    public int maxArea(int[] height) {
+        int result = 0;
+
+        int left = 0;
+        int right = height.length-1;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                result = Math.max(result, height[left] * (right - left));
+                left++;
+            } else {
+                result = Math.max(result, height[right] * (right - left));
+                right--;
+            }
+        }
+        return result;
+    }
+
 }
---
+++ LeetCode/src/com/company/trappingRainWater.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class trappingRainWater {
+
+    public int trap(int[] height) {
+
+        if (height.length < 1) {
+            return 0;
+        }
+
+        int result = 0;
+
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+
+
+
+
+
+
+        return result;
+
+
+    }
+
+}
