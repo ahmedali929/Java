a03c65431e477f6c32e9c275b47fa905bdb02001
ahmedali <ahmed.ali3509@gmail.com>
2025-04-11 23:42:20 -0400

    adding first spring app

---
+++ groovyAppOne/src/main/java/com/company/springboot/demo/groovyAppOne/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.company.springboot.demo.groovyAppOne.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    //expose "/" that will return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+ }
