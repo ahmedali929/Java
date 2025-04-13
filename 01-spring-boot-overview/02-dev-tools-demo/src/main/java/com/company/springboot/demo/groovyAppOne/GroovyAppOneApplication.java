813057cd8fd84240a7f7072c84ce367735830cd0
ahmedali <ahmed.ali3509@gmail.com>
2025-04-13 11:45:58 -0400

    adding new spring project

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/company/springboot/demo/groovyAppOne/GroovyAppOneApplication.java
@@ -0,0 +1,13 @@
+package com.company.springboot.demo.groovyAppOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyAppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyAppOneApplication.class, args);
+	}
+
+}
