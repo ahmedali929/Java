287e39868f3e7fd449e28c6e709702a172bb2a93
ahmedali <ahmed.ali3509@gmail.com>
2025-04-18 09:11:06 -0400

    created test app that adds security to actuator endpoints

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/company/springboot/demo/groovyAppOne/SpringAppFour.java
@@ -0,0 +1,13 @@
+package com.company.springboot.demo.groovyAppOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppFour {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppFour.class, args);
+	}
+
+}
---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/test/java/com/company/springboot/demo/groovyAppOne/SpringAppFourTests.java
@@ -0,0 +1,13 @@
+package com.company.springboot.demo.groovyAppOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppFourTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
