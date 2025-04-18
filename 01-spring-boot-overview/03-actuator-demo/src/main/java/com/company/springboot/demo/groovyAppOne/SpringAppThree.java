f100508b2d746d9c0a3caecdcae476dad9f346a4
ahmedali <ahmed.ali3509@gmail.com>
2025-04-18 08:59:39 -0400

    added more endpoints

--- 01-spring-boot-overview/03-actuator-demo/src/main/java/com/company/springboot/demo/groovyAppOne/SpringAppThree.java
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/company/springboot/demo/groovyAppOne/SpringAppThree.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroovyAppOneApplication {
+public class SpringAppThree {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroovyAppOneApplication.class, args);
+		SpringApplication.run(SpringAppThree.class, args);
 	}
 
 }
--- 01-spring-boot-overview/03-actuator-demo/src/test/java/com/company/springboot/demo/groovyAppOne/SpringAppThreeTests.java
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/company/springboot/demo/groovyAppOne/SpringAppThreeTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GroovyAppOneApplicationTests {
+class SpringAppThreeTests {
 
 	@Test
 	void contextLoads() {
