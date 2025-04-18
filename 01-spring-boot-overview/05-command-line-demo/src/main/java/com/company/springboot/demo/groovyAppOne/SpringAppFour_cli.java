d87ef45bd30017132081e68c0b82d568becc0a3f
ahmedali <ahmed.ali3509@gmail.com>
2025-04-18 13:35:31 -0400

    project to test running the app from the cli

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/company/springboot/demo/groovyAppOne/SpringAppFour_cli.java
@@ -0,0 +1,13 @@
+package com.company.springboot.demo.groovyAppOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppFour_cli {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppFour_cli.class, args);
+	}
+
+}
