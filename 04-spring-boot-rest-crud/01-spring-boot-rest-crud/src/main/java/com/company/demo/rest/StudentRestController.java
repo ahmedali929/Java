45bb3c9a89c83848c1912ce036ea7e2bc19a36a1
ahmedali <ahmed.ali3509@gmail.com>
2025-05-06 23:16:59 -0400

    testing JSON to POJO functionality that is provided by Jackson service

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/company/demo/rest/StudentRestController.java
@@ -0,0 +1,37 @@
+package com.company.demo.rest;
+
+import com.company.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student("Taha", "Shah"));
+        theStudents.add(new Student("Raina", "Shah"));
+        theStudents.add(new Student("Hasim", "Shah"));
+        theStudents.add(new Student("Basim", "Shah"));
+
+        return theStudents;
+    }
+
+
+
+
+
+
+
+
+
+}
