51889dc552499e292d8947a5c34b058d4b47a475
ahmedali <ahmed.ali3509@gmail.com>
2025-05-18 11:14:32 -0400

    created emplopyee service

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package com.company.springboot.cruddemo.rest;
 
-import com.company.springboot.cruddemo.dao.EmployeeDAO;
 import com.company.springboot.cruddemo.entity.Employee;
+import com.company.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +12,17 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     // quick and dirty: inject employee dao
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.company.springboot.cruddemo.service;
+
+import com.company.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+}
