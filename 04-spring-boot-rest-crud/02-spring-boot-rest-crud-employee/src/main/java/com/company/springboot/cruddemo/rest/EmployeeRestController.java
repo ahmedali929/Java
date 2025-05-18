f66cafb6c175273c00d2e58f66477f3bed06d78e
ahmedali <ahmed.ali3509@gmail.com>
2025-05-18 13:46:48 -0400

    added Update and Delete functionality

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
@@ -24,7 +24,6 @@ public class EmployeeRestController {
     }
 
     // add mapping for GET /employees/{employeeId}
-
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
 
@@ -38,7 +37,6 @@ public class EmployeeRestController {
     }
 
     // add mapping for POST /employees - add new employee
-
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee theEmployee) {
 
@@ -48,7 +46,27 @@ public class EmployeeRestController {
         theEmployee.setId(0);
         Employee dbEmployee = employeeService.save(theEmployee);
         return dbEmployee;
+    }
 
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId} - delete existing employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        //throw exception if null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id - " + employeeId;
     }
 
 }
