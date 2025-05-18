40faeeb7b877d36fb3625572a9100b1367629567
ahmedali <ahmed.ali3509@gmail.com>
2025-05-18 13:40:16 -0400

    added Create functionality to create employees

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,10 +2,7 @@ package com.company.springboot.cruddemo.rest;
 
 import com.company.springboot.cruddemo.entity.Employee;
 import com.company.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +35,20 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item .... instead of update
+
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+
     }
 
 }
