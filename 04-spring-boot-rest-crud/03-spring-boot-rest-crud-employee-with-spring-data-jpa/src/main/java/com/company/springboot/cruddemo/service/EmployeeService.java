f28aa6f445db1ff4ff955abaf005b8f6d28db2ea
ahmedali <ahmed.ali3509@gmail.com>
2025-05-19 20:36:50 -0400

    created new Employee service that uses Spring Data JPA

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/company/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.company.springboot.cruddemo.service;
+
+import com.company.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
