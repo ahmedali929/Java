4c5363abfca0b1c1fdea696a15ee59deb1c5b2c7
ahmedali <ahmed.ali3509@gmail.com>
2025-05-19 20:19:05 -0400

    added patch functionality

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/company/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,19 +2,25 @@ package com.company.springboot.cruddemo.rest;
 
 import com.company.springboot.cruddemo.entity.Employee;
 import com.company.springboot.cruddemo.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
+    private ObjectMapper objectMapper;
+
     private EmployeeService employeeService;
 
     // quick and dirty: inject employee dao
-    public EmployeeRestController(EmployeeService theEmployeeService) {
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper) {
         employeeService = theEmployeeService;
+        objectMapper = theObjectMapper;
     }
 
     // expose "/employees" and return a list of employees
@@ -55,6 +61,28 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    // add mapping for PATCH /employees/{employeeId} - patch employhe .... partial update
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        // throw exception if request body contains "id" key
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = apply(patchPayload, tempEmployee);
+
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
     // add mapping for DELETE /employees/{employeeId} - delete existing employee
     @DeleteMapping("/employees/{employeeId}")
     public String deleteEmployee(@PathVariable int employeeId) {
@@ -67,6 +95,20 @@ public class EmployeeRestController {
 
         employeeService.deleteById(employeeId);
         return "Deleted employee id - " + employeeId;
+    }
+
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+        // convert employee object to a JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+        // Convert the patchPayload mpa to a JSON object node
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+        // Merge the patch updates into the employee node
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
     }
 
 }
