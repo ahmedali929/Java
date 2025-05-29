78d6e1686ee36601bb4b49c04dfc19b2d092c92c
Ahmed Ali <ahmedali@Ahmeds-MacBook-Air.local>
2025-05-28 23:32:58 -0500

    adding initbinder to remove white spaces (trailing and leading)

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/company/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/company/springdemo/mvc/CustomerController.java
@@ -1,10 +1,13 @@
 package com.company.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -23,12 +26,20 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
 
+        // Logging last name just to see if it actually exists
+        System.out.println("Last Name: | " + theCustomer.getLastName() + " |");
+
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
             return "customer-confirmation";
         }
+    }
 
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 
 }
