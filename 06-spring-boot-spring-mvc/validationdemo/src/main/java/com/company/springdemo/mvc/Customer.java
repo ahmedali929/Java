9f8d588b42e449aa01343e9116777367caa4ad4d
ahmedali <ahmed.ali3509@gmail.com>
2025-05-30 09:52:49 -0400

    converted FreePasses from int to Integer, and added custom error message for typeMismatch error

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/company/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/company/springdemo/mvc/Customer.java
@@ -12,9 +12,11 @@ public class Customer {
 
     @Min(value=0, message="must be greater than or equal to 0")
     @Max(value=10, message="must be less than or equal to 10")
-    private int freePasses;
+    @NotNull(message="is required")
+    private Integer freePasses;
 
     @Pattern(regexp="^[a-zA-z0-9]{5}", message="only 5 chars/digits")
+    @NotNull(message="is required")
     private String postalCode;
 
     public String getFirstName() {
@@ -33,11 +35,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
