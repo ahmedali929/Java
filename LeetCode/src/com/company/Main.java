47e71117dbbecb6c905682ad7ad5b31e99e405f5
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-06-02 23:16:26 -0500

    starting the kokoEatingBananas leetcode

--- LeetCode/src/com/company/Main.java
+++ LeetCode/src/com/company/Main.java
@@ -5,10 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int[][] matrix = {{1,3,5,7}, {10,11,16,20}, {23,30,34,60}};
-        int target = 3;
 
-        System.out.println(search2dMatrix.searchMatrix(matrix, target));
 
     }
 }
---
+++ LeetCode/src/com/company/kokoEatingBananas.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class kokoEatingBananas {
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+    }
+
+}
