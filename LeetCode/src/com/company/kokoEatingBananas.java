1023a690f6f5c5f1332b933b0e44aeb81a39b09f
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-06-02 23:45:21 -0500

    kokoEatingBananas wip

--- LeetCode/src/com/company/kokoEatingBananas.java
+++ LeetCode/src/com/company/kokoEatingBananas.java
@@ -4,6 +4,29 @@ public class kokoEatingBananas {
 
     public int minEatingSpeed(int[] piles, int h) {
 
+        int result = 0;
+        int minSpeed = 1;
+        int maxSpeed = 0;
+        for (int pile : piles) {
+            maxSpeed = Math.max(maxSpeed, pile);
+        }
+
+        while (minSpeed < maxSpeed) {
+            int mid = minSpeed + (maxSpeed - minSpeed) / 2;
+            int hours = 0;
+            for (int pile : piles) {
+                hours += Math.ceil(pile/mid);
+            }
+            if (hours <= h) {
+                result = Math.min(result, mid);
+                maxSpeed = mid - 1;
+            } else {
+                minSpeed = mid + 1;
+            }
+        }
+
+        return result;
+
     }
 
 }
