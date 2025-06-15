6031315d879411c78d6db4c69ac66028464a6d31
ahmedali <ahmed.ali3509@gmail.com>
2025-06-15 19:20:59 -0400

    created directory for one-to-many jpa mapping project

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-many/src/main/java/com/company/cruddemo/dao/AppDAO.java
@@ -0,0 +1,18 @@
+package com.company.cruddemo.dao;
+
+import com.company.cruddemo.entity.Instructor;
+import com.company.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+}
