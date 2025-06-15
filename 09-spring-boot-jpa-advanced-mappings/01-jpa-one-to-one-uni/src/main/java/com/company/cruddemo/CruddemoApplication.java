435ea408b1cd9f59936aead4cbc11db12feda998
ahmedali <ahmed.ali3509@gmail.com>
2025-06-15 00:07:40 -0400

    created method to retrieve instructor by id

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/company/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/company/cruddemo/CruddemoApplication.java
@@ -18,8 +18,17 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
-			createInstructor(appDAO);
+			findInstructor(appDAO);
 		};
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
 	}
 
 	private void createInstructor(AppDAO appDAO) {
@@ -35,8 +44,7 @@ public class CruddemoApplication {
 
 		// save the instructor
 		//
-		// NOTE: this will ALSO save the details object
-		// because of CascadeType.ALL
+		// NOTE: this will ALSO save the details object because of CascadeType.ALL
 		//
 		System.out.println("Saving instructor: " + tempInstructor);
 		appDAO.save(tempInstructor);
