3c2bb850630a2725fff880a98db19b4f2adaf37b
ahmedali <ahmed.ali3509@gmail.com>
2025-06-27 13:12:50 -0400

    created method to delete courses, which also deletes its associated reviews

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many-uni/src/main/java/com/company/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many-uni/src/main/java/com/company/cruddemo/CruddemoApplication.java
@@ -24,9 +24,22 @@ public class CruddemoApplication {
 		return runner -> {
 
 			//createCourseAndReviews(appDAO);
-			retrieveCourseAndReviews(appDAO);
+			//retrieveCourseAndReviews(appDAO);
+			deleteCourseAndReviews(appDAO);
 
 		};
+	}
+
+	private void deleteCourseAndReviews(AppDAO appDAO) {
+
+		int theId = 10;
+
+		System.out.println("Deleting course id: " + theId);
+
+		appDAO.deleteCourseById(theId); //this will delete the course's reviews as well
+
+		System.out.println("Done!");
+
 	}
 
 	private void retrieveCourseAndReviews(AppDAO appDAO) {
