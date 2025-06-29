f7443d61db7aade417431c912c48b0af46272904
ahmedali <ahmed.ali3509@gmail.com>
2025-06-29 19:32:54 -0400

    setting up first project for AOP

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/test/java/com/company/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
