87bffc8c62caf891fa1971cff293076cd4bcc36a
ahmedali <ahmed.ali3509@gmail.com>
2025-07-01 15:44:37 -0400

    added order to the aspects/advices

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,12 @@ package com.company.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyDemoLoggingAspect {
 
     @Before("com.company.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
