1f9f3ae1b8b28328129dafc09f65ccef4746a35d
ahmedali <ahmed.ali3509@gmail.com>
2025-07-01 15:55:56 -0400

    added JoinPoint to the advice that will help us read arguments/parameters from its methods

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.company.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,17 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.company.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n =====> Executing @Before advice on addAccount()");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+
     }
 
 }
