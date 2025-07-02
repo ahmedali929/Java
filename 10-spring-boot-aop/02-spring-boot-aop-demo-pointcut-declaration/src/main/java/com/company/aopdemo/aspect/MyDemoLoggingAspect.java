a0b072e62db53073efe514035e847ed3e2787d21
ahmedali <ahmed.ali3509@gmail.com>
2025-07-02 14:56:27 -0400

    created afterreturning advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,33 @@ package com.company.aopdemo.aspect;
 
 import com.company.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(3)
 public class MyDemoLoggingAspect {
+
+    // add a new advice for @AfterReturning on the findAccounts method
+    @AfterReturning(pointcut="execution(* com.company.aopdemo.dao.AccountDAO.findAccounts(..))", returning="result")
+    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=======> Executing @AfterReturning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("\n=======> result is: " + result);
+
+    }
+
 
     @Before("com.company.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
