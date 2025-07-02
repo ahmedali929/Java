89c058e5cb7d24fee71a92e707c65592cba660f7
ahmedali <ahmed.ali3509@gmail.com>
2025-07-02 14:36:48 -0400

    created method on application layer to return accounts

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/AopdemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class AopdemoApplication {
 
@@ -19,9 +21,22 @@ public class AopdemoApplication {
 
 		return runner -> {
 
-			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
-
+			//demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			demoTheAfterReturningAdvice(theAccountDAO);
 		};
+
+	}
+
+	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
+
+		// call the method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+
+		// display the accounts
+		System.out.println("\n\n Main Program: demoTheAfterReturningAdvice");
+		System.out.println("-----");
+		System.out.println(theAccounts);
+		System.out.println("\n");
 
 	}
 
