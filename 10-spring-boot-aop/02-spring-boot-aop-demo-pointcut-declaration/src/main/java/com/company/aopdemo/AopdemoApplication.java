b189f3f7304bd2dbc7a134a2b453f783cee69332
ahmedali <ahmed.ali3509@gmail.com>
2025-07-03 11:06:02 -0400

    created a method in the main application that calls the trafficFortuneService

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/company/aopdemo/AopdemoApplication.java
@@ -2,6 +2,7 @@ package com.company.aopdemo;
 
 import com.company.aopdemo.dao.AccountDAO;
 import com.company.aopdemo.dao.MembershipDAO;
+import com.company.aopdemo.service.TrafficFortuneService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,16 +18,27 @@ public class AopdemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO, TrafficFortuneService theTrafficFortuneService) {
 
 		return runner -> {
 
 			//demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
 			//demoTheAfterReturningAdvice(theAccountDAO);
 			//demoTheAfterThrowingAdvice(theAccountDAO);
-			
-			demoTheAfterAdvice(theAccountDAO);
+			//demoTheAfterAdvice(theAccountDAO);
+			demoTheAroundAdvice(theTrafficFortuneService);
+
 		};
+
+	}
+
+	private void demoTheAroundAdvice(TrafficFortuneService theTrafficFortuneService) {
+
+		System.out.println("\nMain Program: demoTheAroundAdvice");
+		System.out.println("Calling getFortune()");
+		String data = theTrafficFortuneService.getFortune();
+		System.out.println("\nMy fortune is: " + data);
+		System.out.println("Finished");
 
 	}
 
