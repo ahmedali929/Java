fb7ab16f06e6f4e2d6207d3f0eb17c132d0accbc
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-11-21 15:46:11 -0500

    added display names to the test methods

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -30,15 +30,15 @@ class DemoUtilsTest {
     }
 
     @Test
+    @DisplayName("Equals and Not Equals")
     void testEqualsAndNotEquals() {
-        System.out.println("Running test: testEqualsAndNotEquals");
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
         assertNotEquals(6,demoUtils.add(1,9),"1+9 must not be 6");
     }
 
     @Test
+    @DisplayName("Null and Not Null")
     void testNullAndNotNull() {
-        System.out.println("Running test: testNullAndNotNull");
         String str1 = null;
         String str2 = "luv2code";
         assertNull(demoUtils.checkNull(str1), "Object should be null");
