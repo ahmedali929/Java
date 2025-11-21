a0a9b34a76e9a96335d786a0b5f0ded1fff882d7
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-11-21 16:07:54 -0500

    added test cases for iterables and line matching

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
@@ -60,6 +62,27 @@ class DemoUtilsTest {
         int gradeTwo = 5;
         assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
         assertFalse(demoUtils.isGreater(gradeTwo,gradeOne), "This should return false");
+    }
+
+    @Test
+    @DisplayName("Array Equals")
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be equal");
+    }
+
+    @Test
+    @DisplayName("Iterable Equals")
+    void testIterableEquals() {
+        List<String> theList = List.of("luv","2","code");
+        assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be the same as the actual list");
+    }
+
+    @Test
+    @DisplayName("Lines match")
+    void testLinesMatch() {
+        List<String> theList = List.of("luv","2","code");
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
     }
 
 }
