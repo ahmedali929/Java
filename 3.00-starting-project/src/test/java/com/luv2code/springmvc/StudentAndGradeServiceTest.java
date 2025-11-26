c772b590e0d07ffe7a63f59059979910d78bedef
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-11-26 13:57:23 -0500

    added studentdao and autowired it in the test class

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,6 +1,7 @@
 package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
     @Autowired
     private StudentAndGradeService studentService;
+
+    @Autowired
+    private StudentDao studentDao;
 
     @Test
     @DisplayName("Basic test")
