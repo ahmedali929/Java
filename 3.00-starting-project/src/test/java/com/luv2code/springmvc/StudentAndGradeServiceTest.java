e78b8bfe3bb35c04696131bef3cb5ec510f93335
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-11-26 14:39:51 -0500

    fixed typo in gradebook test

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -80,7 +80,7 @@ public class StudentAndGradeServiceTest {
             collegeStudents.add(collegeStudent);
         }
 
-        assertEquals(1, collegeStudents.size());
+        assertEquals(5, collegeStudents.size());
     }
 
 }
