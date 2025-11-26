2755dbac891eced72ada3b02f1cc3f60a94a8ecd
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-11-26 14:17:22 -0500

    added beforeeach and aftereach methods that setup and remove database for tests

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -3,10 +3,13 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,10 +19,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StudentAndGradeServiceTest {
 
     @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
     private StudentAndGradeService studentService;
 
     @Autowired
     private StudentDao studentDao;
+
+    @BeforeEach
+    public void setupDatabase() {
+        jdbc.execute("INSERT INTO student(firstname, lastname, email_address) VALUES ('Eric', 'Roby','eric.roby@luv2code.com')");
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+        jdbc.execute("ALTER TABLE student ALTER COLUMN ID RESTART WITH 1");
+    }
 
     @Test
     @DisplayName("Basic test")
