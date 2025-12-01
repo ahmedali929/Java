52b1d728d375723e0604645a6d77edf670e84567
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-12-01 18:09:55 -0500

    updated tests and dao methods to deleted associated grades with students when a student is deleted

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/HistoryGradesDao.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/HistoryGradesDao.java
@@ -4,4 +4,5 @@ import com.luv2code.springmvc.models.HistoryGrade;
 import org.springframework.data.repository.CrudRepository;
 public interface HistoryGradesDao extends CrudRepository<HistoryGrade, Integer> {
             public Iterable<HistoryGrade> findGradeByStudentId(int id);
+            public void deleteByStudentId(int id);
         }
