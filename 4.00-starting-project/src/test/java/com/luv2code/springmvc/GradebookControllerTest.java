4d4bdacdf328a81193e8288ed7ae51df5c25b92c
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-12-07 14:27:39 -0500

    created test for deleting a grade

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -2,6 +2,7 @@ package com.luv2code.springmvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.MathGrade;
 import com.luv2code.springmvc.repository.HistoryGradesDao;
 import com.luv2code.springmvc.repository.MathGradesDao;
 import com.luv2code.springmvc.repository.ScienceGradesDao;
@@ -231,6 +232,21 @@ public class GradebookControllerTest {
                 .andExpect(status().is4xxClientError())
                 .andExpect(jsonPath("$.status", is(404)))
                 .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
+    }
+
+    @Test
+    public void deleteAValidGradeHttpRequest() throws Exception {
+        Optional<MathGrade> mathGrade = mathGradeDao.findById(1);
+        assertTrue(mathGrade.isPresent());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/grades/{id}/{gradeType}", 1, "math"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstname",is( "Taha")))
+                .andExpect(jsonPath("$.lastname", is("Shah")))
+                .andExpect(jsonPath("$.emailAddress", is("tahashah@tahashah.com")))
+                .andExpect(jsonPath("$.studentGrades.mathGradeResults", hasSize(0)));
+
     }
 
 
