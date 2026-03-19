2bb5d0b71d474e019091e1835f8e422dc76ea5c6
ahmedali <ahmed.ali3509@gmail.com>
2026-03-19 00:15:46 -0400

    mapped the element collection

--- solution-code-hibernate-hb-01-map-collections-set/src/main/java/com/company/hibernate/demo/entity/Student.java
+++ solution-code-hibernate-hb-01-map-collections-set/src/main/java/com/company/hibernate/demo/entity/Student.java
@@ -1,6 +1,8 @@
 package com.company.hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="student")
@@ -18,6 +20,11 @@ public class Student {
 
     @Column(name="email")
     private String email;
+
+    @ElementCollection
+    @CollectionTable(name="image", joinColumns = @JoinColumn(name="student_id"))
+    @Column(name="file_name") //defaults to images
+    private Set<String> images = new HashSet<String>();
 
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
@@ -55,6 +62,14 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<String> getImages() {
+        return images;
+    }
+
+    public void setImages(Set<String> images) {
+        this.images = images;
     }
 
     @Override
