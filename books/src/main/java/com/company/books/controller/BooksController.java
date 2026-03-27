805d0da3a8bdbcc77dd3f5a645e0cdba0a986f1d
ahmedali <ahmed.ali3509@gmail.com>
2026-03-26 22:49:30 -0400

    created books entity and updated BooksController to generate and return books

--- books/src/main/java/com/company/books/controller/BooksController.java
+++ books/src/main/java/com/company/books/controller/BooksController.java
@@ -1,13 +1,35 @@
 package com.company.books.controller;
 
+import com.company.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BooksController {
 
-    @GetMapping("/api")
-    public String firstAPI() {
-        return "Hello World";
+    private final List<Book> books = new ArrayList<>();
+
+    public BooksController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+        new Book("Title One", "Author One", "science"),
+        new Book("Title Two", "Author Two", "history"),
+        new Book("Title Three", "Author Three", "math"),
+        new Book("Title Four", "Author Four", "ethics"),
+        new Book("Title Five", "Author Five", "religion"),
+        new Book("Title Six", "Author Six", "philosophy"),
+        new Book("Title Seven", "Author Seven", "politics")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
     }
 }
