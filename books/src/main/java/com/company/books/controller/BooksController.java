f04105ed0e3d6a5c615d5944cc6bcb784e11b5ad
ahmedali <ahmed.ali3509@gmail.com>
2026-04-01 02:12:29 -0400

    created method to updated books

--- books/src/main/java/com/company/books/controller/BooksController.java
+++ books/src/main/java/com/company/books/controller/BooksController.java
@@ -59,4 +59,14 @@ public class BooksController {
             books.add(newBook);
         }
     }
+
+    @PutMapping("/api/books/{title}")
+    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getTitle().equalsIgnoreCase(title)) {
+                books.set(i, updatedBook);
+                return;
+            }
+        }
+    }
 }
