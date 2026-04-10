bdd15ab447e96f305d11b35b5d51896353f6b7a6
ahmedali <ahmed.ali3509@gmail.com>
2026-04-10 10:41:48 -0400

    updated pom and properties to use swagger and validations

--- employees/src/main/java/com/company/springboot/employees/request/EmployeeRequest.java
+++ employees/src/main/java/com/company/springboot/employees/request/EmployeeRequest.java
@@ -1,9 +1,14 @@
 package com.company.springboot.employees.request;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeRequest {
 
-    @NotBlank @Size(min=2, max=50)
+    @NotBlank
+    @Size(min=2, max=50)
     private String firstName;
 
     @NotBlank @Size(min=2, max=50)
