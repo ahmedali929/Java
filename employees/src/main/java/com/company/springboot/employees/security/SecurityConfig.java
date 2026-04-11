a73c19a113e3acee8ce779677f503b3b0f880c4d
ahmedali <ahmed.ali3509@gmail.com>
2026-04-11 18:28:43 -0400

    added security filter chain that will implement role-based permissions

--- employees/src/main/java/com/company/springboot/employees/security/SecurityConfig.java
+++ employees/src/main/java/com/company/springboot/employees/security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.company.springboot.employees.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,7 +35,25 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
 
+    @Bean
+    public SecurityFilterChain filerChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN"));
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 
 }
