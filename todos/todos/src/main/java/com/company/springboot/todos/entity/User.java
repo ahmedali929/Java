22104b4502fa0b4b3434f603b9ab20bb64b62b47
ahmedali <ahmed.ali3509@gmail.com>
2026-04-11 20:48:35 -0400

    updated user constructor to not need authorities

--- todos/todos/src/main/java/com/company/springboot/todos/entity/User.java
+++ todos/todos/src/main/java/com/company/springboot/todos/entity/User.java
@@ -46,13 +46,12 @@ public class User implements UserDetails {
 
     public User() {};
 
-    public User(long id, String firstName, String lastName, String email, String password, List<Authority> authorities, List<Todo> todos) {
+    public User(long id, String firstName, String lastName, String email, String password, List<Todo> todos) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.authorities = authorities;
         this.todos = todos;
     }
 
