32cdca022494924fd50a1adb0d250380f26cef47
ahmedali <ahmed.ali3509@gmail.com>
2026-04-18 11:45:22 -0400

    set up security configs for the app

---
+++ todos/todos/src/main/java/com/company/springboot/todos/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.company.springboot.todos.repository;
+
+import com.company.springboot.todos.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
