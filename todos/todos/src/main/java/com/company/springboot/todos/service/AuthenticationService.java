2f899792121edbb3f102b36761ad907e17cfcf20
ahmedali <ahmed.ali3509@gmail.com>
2026-04-28 13:50:16 -0400

    updated auth service to use tokens to authenticate logins

--- todos/todos/src/main/java/com/company/springboot/todos/service/AuthenticationService.java
+++ todos/todos/src/main/java/com/company/springboot/todos/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package com.company.springboot.todos.service;
 
+import com.company.springboot.todos.request.AuthenticationRequest;
 import com.company.springboot.todos.request.RegisterRequest;
+import com.company.springboot.todos.response.AuthenticationResponse;
 
 public interface AuthenticationService {
     void register(RegisterRequest input) throws Exception;
+    AuthenticationResponse login(AuthenticationRequest request);
 }
--- todos/todos/src/main/java/com/company/springboot/todos/service/AuthenticationServiceImpl.java
+++ todos/todos/src/main/java/com/company/springboot/todos/service/AuthenticationServiceImpl.java
@@ -1,13 +1,19 @@
 package com.company.springboot.todos.service;
 
-import com.company.springboot.todos.entity.*;
+import com.company.springboot.todos.entity.Authority;
+import com.company.springboot.todos.entity.User;
 import com.company.springboot.todos.repository.UserRepository;
+import com.company.springboot.todos.request.AuthenticationRequest;
 import com.company.springboot.todos.request.RegisterRequest;
+import com.company.springboot.todos.response.AuthenticationResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -16,10 +22,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @Override
@@ -32,6 +42,21 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         User user = buildNewUser(input);
         userRepository.save(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public AuthenticationResponse login(AuthenticationRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password"));
+
+        String jwtToken = jwtService.generateToken(new HashMap<>(), user);
+
+        return new AuthenticationResponse(jwtToken);
     }
 
     private boolean isEmailTaken(String email) {
