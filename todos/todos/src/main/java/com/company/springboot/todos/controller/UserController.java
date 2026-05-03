6f879875ca6fe2076075018402a734307d01a79f
ahmedali <ahmed.ali3509@gmail.com>
2026-05-02 21:26:54 -0400

    added operation and response status tags to user controller endpoints

--- todos/todos/src/main/java/com/company/springboot/todos/controller/UserController.java
+++ todos/todos/src/main/java/com/company/springboot/todos/controller/UserController.java
@@ -3,8 +3,10 @@ package com.company.springboot.todos.controller;
 import com.company.springboot.todos.request.PasswordUpdateRequest;
 import com.company.springboot.todos.response.UserResponse;
 import com.company.springboot.todos.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
@@ -20,16 +22,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "User information", description = "Get current user info")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo() throws AccessDeniedException {
         return userService.getUserInfo();
     }
 
+    @Operation(summary = "Delete User", description = "Delete current user account")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
     public void deleteUser() throws AccessDeniedException {
         userService.deleteUser();
     }
 
+    @Operation(summary = "Password update", description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/password")
     public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) throws Exception{
         userService.updatePassword(passwordUpdateRequest);
