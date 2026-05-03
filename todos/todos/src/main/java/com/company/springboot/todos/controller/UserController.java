af6a9fc21d83534442c1ab499396f851da2ae92f
ahmedali <ahmed.ali3509@gmail.com>
2026-05-02 20:45:19 -0400

    added user deletion method to user controller

--- todos/todos/src/main/java/com/company/springboot/todos/controller/UserController.java
+++ todos/todos/src/main/java/com/company/springboot/todos/controller/UserController.java
@@ -3,6 +3,7 @@ package com.company.springboot.todos.controller;
 import com.company.springboot.todos.response.UserResponse;
 import com.company.springboot.todos.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class UserController {
     @GetMapping("/info")
     public UserResponse getUserInfo() throws AccessDeniedException {
         return userService.getUserInfo();
+    }
+
+    @DeleteMapping
+    public void deleteUser() throws AccessDeniedException {
+        userService.deleteUser();
     }
 }
