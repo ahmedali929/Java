1b5318237cb033dd46ad3784993b9996483a357b
ahmedali <ahmed.ali3509@gmail.com>
2026-05-02 20:25:23 -0400

    updating security configs to allow swagger to use jwt's

--- todos/todos/src/main/java/com/company/springboot/todos/config/SecurityConfig.java
+++ todos/todos/src/main/java/com/company/springboot/todos/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer.requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resourcesx/**", "/webjars/**", "/docs").permitAll());
+        http.authorizeHttpRequests(configurer -> configurer.requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resourcesx/**", "/webjars/**", "/docs").permitAll().anyRequest().authenticated());
 
         http.csrf(csrf -> csrf.disable());
 
