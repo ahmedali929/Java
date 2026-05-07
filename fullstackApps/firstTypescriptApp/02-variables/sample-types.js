8886341a865406c59119318a04d8c921ec72692d
ahmedali <ahmed.ali3509@gmail.com>
2026-05-07 10:10:50 -0400

    created variables file

---
+++ fullstackApps/firstTypescriptApp/02-variables/sample-types.js
@@ -0,0 +1,8 @@
+"use strict";
+let found = true;
+let grade = 88.6;
+let firstName = "Taha";
+let lastName = "Shah";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
