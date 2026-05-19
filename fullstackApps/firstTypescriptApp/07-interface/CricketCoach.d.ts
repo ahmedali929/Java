12cf55c68e2a375d4bc64bd44c60efc78095da6a
ahmedali <ahmed.ali3509@gmail.com>
2026-05-19 18:07:03 -0400

    practicing with interfaces in typescript

---
+++ fullstackApps/firstTypescriptApp/07-interface/CricketCoach.d.ts
@@ -0,0 +1,5 @@
+import { Coach } from "./Coach";
+export declare class CricketCoach implements Coach {
+    getDailyWorkout(): string;
+}
+//# sourceMappingURL=CricketCoach.d.ts.map
---
+++ fullstackApps/firstTypescriptApp/07-interface/CricketCoach.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CricketCoach = void 0;
+class CricketCoach {
+    getDailyWorkout() {
+        return "Practice your bowling";
+    }
+}
+exports.CricketCoach = CricketCoach;
+//# sourceMappingURL=CricketCoach.js.map
